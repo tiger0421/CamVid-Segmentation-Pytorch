8858e078716d6704b9e98074fcaeb715a76027b2
YusukeKiyooka <s17c1045kh@s.chibakoudai.jp>
2021-06-09 20:01:23 +0900

    add: run on ROS forcely

--- U-Net/test.py
+++ U-Net/test.py
@@ -9,31 +9,50 @@ from src.utils import *
 from src.Camvid import *
 from src.IoU import *
 from src.eval import *
+# ROS module
+import rospy
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
 
-CONFIG = config()
-path = CONFIG.path
-batch = CONFIG.batch
-input_size = CONFIG.input_size
-load_model_pth = CONFIG.load_model
-device = CONFIG.device
+class MyUnet:
+    def __init__(self):
+        self.pub = rospy.Publisher('segmentated_image', Image, queue_size=10)
+        self.CONFIG = config()
+        self.path = CONFIG.path
+        self.batch = CONFIG.batch
+        self.input_size = CONFIG.input_size
+        self.load_model_pth = CONFIG.load_model
+        self.device = CONFIG.device
+
+        self.transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize(self.input_size, 0),
+            transforms.ToTensor(),
+        ])
+        self.model = UNet(3, 32, True).to(self.device)
+
+
+    def callback(self, msg):
+        try:
+            image_orig = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
+        except CvBridgeError as e:
+            print(e)
+        image = cv2.cvtColor(image_orig, cv2.COLOR_BGR2RGB)
+        image = self.transform(image)
+        _, height, width = image.shape
+        image = image.view(1, 3, height, width)
+
+        pred = myTest_eval(self.model, image, self.load_model_pth, self.device)
+        pred = cv2.cvtColor(pred, cv2.COLOR_RGB2BGR)
+        try:
+            imageMsg = bridge.cv2_to_imgmsg(pred, "bgr8")
+            self.pub.publish(imageMsg)
+        except Exception as e:
+            print(e)
+        
 
 if __name__ == "__main__":
-    transform = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize(input_size, 0),
-        transforms.ToTensor(),
-    ])
-
-    image_orig = cv2.imread("/root/share/CamVid-Segmentation-Pytorch/images/real_line2.jpg", cv2.IMREAD_COLOR)
-    image_orig = image_orig[:1024, :1024, :]
-    image = cv2.cvtColor(image_orig, cv2.COLOR_BGR2RGB)
-    image = transform(image)
-    _, height, width = image.shape
-    image = image.view(1, 3, height, width)
-
-    model = UNet(3, 32, True).to(device)
-
-    pred = myTest_eval(model, image, load_model_pth, device)
-    pred = cv2.cvtColor(pred, cv2.COLOR_RGB2BGR)
-    cv2.imwrite("input.png", image_orig)
-    cv2.imwrite("result.png", pred)
+    rospy.init_node('UNet')
+    myUnet = MyUnet()
+    rospy.Subscriber("image_raw", Image, myUnet.callback)
+    rospy.spin()
